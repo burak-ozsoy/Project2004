faefd6a4e389e70a50eb6b219333ff8552ff4a6d
Volkan Alkan <volkanalkan2001@hotmail.com>
2024-05-18 17:56:26 +0300

    AnalogClockLearnActivity (will be continuing...)

--- app/src/main/java/com/example/project2004v2/learn/AnalogClockLearnActivity.java
+++ app/src/main/java/com/example/project2004v2/learn/AnalogClockLearnActivity.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.project2004v2.AnalogClockActivity;
 import com.example.project2004v2.R;
+import com.example.project2004v2.AnalogClockActivity;
 
 public class AnalogClockLearnActivity extends AppCompatActivity {
 
+    private EditText timeInput;
+    private TextView resultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analog_clock_learn);
 
+        timeInput = findViewById(R.id.time_input);
+        resultText = findViewById(R.id.result_text);
+        Button btnCheckTime = findViewById(R.id.btn_check_time);
         Button btnGoBack = findViewById(R.id.btn_go_back);
+
+        btnCheckTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkTime();
+            }
+        });
+
         btnGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,7 +40,19 @@ public class AnalogClockLearnActivity extends AppCompatActivity {
                 finish();
             }
         });
-
     }
 
+    private void checkTime() {
+        String inputTime = timeInput.getText().toString().trim();
+        // Örnek doğru zaman: 10:15
+        String correctTime = "10:15";
+
+        if (inputTime.equals(correctTime)) {
+            resultText.setText("Correct! The time is " + correctTime);
+            resultText.setTextColor(getResources().getColor(R.color.correct_color));
+        } else {
+            resultText.setText("Try again! The correct time is " + correctTime);
+            resultText.setTextColor(getResources().getColor(R.color.error_color));
+        }
+    }
 }
